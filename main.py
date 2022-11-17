5060369a0cea1822223bfbf13df5f917c26739c6
Tarun <starun.shan@gmail.com>
2022-11-17 10:49:03 +0530

    first comment

---
+++ main.py
@@ -0,0 +1,84 @@
+from fastapi import FastAPI, Body, Depends
+import schemas
+import models
+
+from database import Base,engine, SessionLocal
+from sqlalchemy.orm import Session
+Base.metadata.create_all(engine)
+
+
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+
+    finally:
+            session.close()
+
+
+
+app = FastAPI()
+
+fakeDatabase ={
+    1:{'task':'Clean car'},
+        2:{'task':'make notes'},
+            3:{'task':'take sim'},
+
+
+}
+
+@app.get("/")
+def getItems(session: Session = Depends(get_session)):
+    items = session.query(models.Item).all()
+    return items
+
+
+
+@app.get("/{id}")
+def getItem(id:int,session: Session = Depends(get_session)):
+    item = session.query(models.Item).get(id)
+    return item
+
+
+
+# @app.post("/")
+
+# def addItem(task:str):
+    
+#     newId = len(fakeDatabase.keys()) + 1
+#     fakeDatabase[newId]={"task":task}
+#     return fakeDatabase
+
+
+@app.post("/")
+
+def addItem(item:schemas.Item,session: Session = Depends(get_session)):
+    
+    item = models.Item(task = item.task)
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+   
+    return item
+
+@app.put("/{id}")
+def updateItem(id:int, item:schemas.Item,session: Session = Depends(get_session)):
+     itemObject = session.query(models.Item).get(id)
+     itemObject.task = item.task
+     session.commit()
+     return itemObject
+
+
+@app.delete("/{id}")
+def deleteItem(id:int,session: Session = Depends(get_session)):
+     itemObject = session.query(models.Item).get(id)
+     session.delete(itemObject)
+     session.commit()
+     session.close()
+     return 'Item was deleted'
+      
+
+
+
+
+
